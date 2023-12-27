922a060ee7314727c6100e674902728a669f2023
dpsi9 <dipendras953@gmail.com>
2023-12-27 19:37:08 +0530

    Added video model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-
 import connectDB from "./db/index.js";
-
+import app from "./app.js"
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`Server running on ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB failed to connect !!! ",error);
+})
